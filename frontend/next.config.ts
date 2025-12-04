1b248b9174a85f4a23376b6d31523bfc64916e59
yash khude <khudeyash2021@gmail.com>
2025-12-04 12:31:03 +0530

    Update Files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
